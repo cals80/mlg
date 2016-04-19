fe967441f73f7b56eadad80cbe79094a6201809f
cals80 <souza.capple@gmail.com>
2016-04-19 19:20:25 -0300

    UserDAO working

--- src/main/scala/com/carlossouza/Main.scala
+++ src/main/scala/com/carlossouza/Main.scala
@@ -1,5 +1,11 @@
 package com.carlossouza
 
+import java.sql.Timestamp
+import java.util.Calendar
+
+import com.carlossouza.dao._
+import com.carlossouza.models.UserRow
+import org.joda.time.DateTime
 import play.api.libs.json.JsValue
 import play.api.libs.ws.ning.{NingWSClient}
 import scala.concurrent.Future
@@ -7,13 +13,15 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Success, Failure}
 import play.api.libs.json.Json
 
+import scala.concurrent.{Future, Await}
+import slick.driver.MySQLDriver.api._
+import scala.concurrent.duration._
+
 /**
   * Created by carlossouza on 4/16/16.
   */
 
 object Main extends App {
-
-  println(args)
 
   val wsClient = NingWSClient()
   val APIKey = "4d6f643856f412a263841233916952"
@@ -21,7 +29,7 @@ object Main extends App {
 
   val futureResult = meetupSource.getCities   //.getMembers(18804321) //.getCities
 
-  futureResult.onComplete {
+  /*futureResult.onComplete {
     case Success(m) => {
       println(m)
       //println(Json.prettyPrint(m))
@@ -31,5 +39,25 @@ object Main extends App {
       println("An error has occured: " + t.getMessage)
       wsClient.close()
     }
+  }*/
+  futureResult.map { result =>
+    println(result)
+    wsClient.close()
   }
+
+  Await.result(futureResult, Duration.Inf)
+
+
+  val currentTimestamp = new Timestamp(Calendar.getInstance.getTime.getTime)
+
+  println("\nBeginning MySQL part")
+  val db = Database.forConfig("localhost")
+  val userDAO = new UserDAO(db)
+  val f = userDAO.deleteById(7)
+  f.map { result =>
+    println(result)
+    db.close()
+  }
+  Await.ready(f, Duration.Inf)
+
 }
---
+++ src/main/scala/com/carlossouza/dao/UserDAO.scala
@@ -0,0 +1,31 @@
+package com.carlossouza.dao
+
+import javax.inject.Inject
+
+import com.carlossouza.models.{UserRow, Tables}
+
+import slick.driver.MySQLDriver.api._
+import slick.driver.MySQLDriver
+import scala.concurrent._
+
+
+/**
+  * Created by carlossouza on 4/19/16.
+  */
+class UserDAO @Inject() (db: MySQLDriver.backend.Database) {
+
+  def fetchAll(): Future[Seq[UserRow]] = db.run(Tables.User.result)
+
+  def count(): Future[Int] = db.run(Tables.User.length.result)
+
+  def insert(newRow: UserRow): Future[Int] = db.run((Tables.User returning Tables.User.map(_.id)) += newRow)
+
+  def save(maybeNewRow: UserRow): Future[Int] = db.run(Tables.User.insertOrUpdate(maybeNewRow))
+
+  def findById(id: Int): Future[Option[UserRow]] = db.run(Tables.User.filter(_.id === id).result.headOption)
+
+  def deleteById(id: Int): Future[Int] = db.run(Tables.User.filter(_.id === id).delete)
+
+  def update(updatedRow: UserRow): Future[Int] = db.run(Tables.User.filter(_.id === updatedRow.id).update(updatedRow))
+
+}
